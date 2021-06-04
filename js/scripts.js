60544c0cb6952ca501e9e197e434ee242f7d3c4e
cellnoob <60471273+cellnoob@users.noreply.github.com>
2021-06-04 11:10:53 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+console.log("connecting!...");
+
+$(document).ready(function () {
+  $("#callToAction").click(function (e) {
+    e.preventDefault();
+    $.getJSON(
+      "http://api.icndb.com/jokes/random",
+      function (data, textStatus, jqXHR) {
+        console.log(data);
+        console.log(textStatus);
+        console.log(jqXHR);
+        $("#display").html(`${data.value.joke}`);
+      }
+    );
+  });
+});
+
+// fetch method print in console
+fetch("http://api.icndb.com/jokes/random",{
+    method: "POST",
+    body:"data"
+})
+.then(function(response){
+    if(response.ok){
+        return response.text();
+    }else{
+        throw "Call error";
+    }
+})
+.then(function(texto){
+    console.log(texto);
+})
+.catch(function(error){
+    console.log(error);
+})
+
+
+
